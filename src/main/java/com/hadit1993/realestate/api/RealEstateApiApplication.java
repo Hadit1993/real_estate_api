37fb3a979a241a2c16dd1e9eb0354d2e0ecd92cc
hadit1993 <hadit1993@gmail.com>
2023-12-10 20:43:05 +0330

    Create and run the server

---
+++ src/main/java/com/hadit1993/realestate/api/RealEstateApiApplication.java
@@ -0,0 +1,13 @@
+package com.hadit1993.realestate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadit1993/realestate/api/RealEstateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadit1993.realestate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
